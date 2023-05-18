ccff01830555654a0ffc9ef70b4969010a7242f8
damardanangsa <133944985+damardanangsa@users.noreply.github.com>
2023-05-18 12:43:47 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="id">
+	<head>
+        <meta name="robots" content="index,follow">
+        <meta name="googlebot" content="index,follow"/>
+        <meta name="language" content="Indonesia">
+        <title>SITUS SEDANG MAINTENANCE</title>
+        <head>
+        <body>
+        <center><h1>SITUS SEDANG MAINTENANCE</h1></center>
+	</body>
+</html>
